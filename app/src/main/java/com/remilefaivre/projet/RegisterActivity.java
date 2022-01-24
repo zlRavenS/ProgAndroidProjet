6368fb4587f9c9d9f7337c63473bfee87db62907
Deschamps Antonin <39429978+Ssilvers@users.noreply.github.com>
2022-01-24 13:37:35 +0100

    ajout login/register
    Nom du compte: deschamps
    mail: ad@gmail.com
    mdp: 123456789

--- app/src/main/java/com/remilefaivre/projet/RegisterActivity.java
+++ app/src/main/java/com/remilefaivre/projet/RegisterActivity.java
@@ -10,53 +10,50 @@ import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.androidnetworking.interfaces.OkHttpResponseListener;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
+
+        AndroidNetworking.initialize(this);
     }
 
-    public void onClickInscrire(View view) {
-        Intent i = new Intent(MainActivity.this, RegisterActivity.class);
-        startActivity(i);
-    }
-
-    public void logging(View view) {
+    public void register(View view) {
+        final EditText champNom = (EditText) findViewById(R.id.txt_name);
+        String name = champNom.getText().toString();
         final EditText champEmail = (EditText) findViewById(R.id.txt_mail);
         String email = champEmail.getText().toString();
         final EditText champMdp = (EditText) findViewById(R.id.txt_password);
         String mdp = champMdp.getText().toString();
 
-        AndroidNetworking.post("https://myhouse.lesmoulinsdudev.com/auth")
+        AndroidNetworking.post("https://myhouse.lesmoulinsdudev.com/register")
+                .addBodyParameter("name", name)
                 .addBodyParameter("login", email)
                 .addBodyParameter("password", mdp)
                 .build()
-                .getAsJSONObject(new JSONObjectRequestListener() {
+                .getAsOkHttpResponse(new OkHttpResponseListener() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            String token = response.getString("token");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                    public void onResponse(Response response) {
+                        switch (response.code()) {
+                            case 200:
+                                Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                                startActivity(i);
+                                break;
+                            default:
                         }
-
-                        Intent i = new Intent(MainActivity.this, RoomsActivity.class);
-                        startActivity(i);
                     }
 
                     @Override
                     public void onError(ANError anError) {
-                        Toast toast = Toast.makeText(MainActivity.this, "Erreur", Toast.LENGTH_SHORT);
+                        Toast toast = Toast.makeText(RegisterActivity.this, "Erreur", Toast.LENGTH_SHORT);
                         toast.show();
                     }
                 });
-
     }
 }
